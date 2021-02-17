58939fbf7e85ed3547cd168add55057f7bd42974
kuberlog <collinalexbell@gmail.com>
2021-02-16 20:18:38 -0600

    Goals have tags now, they save in the database, mongo

--- goal.py
+++ goal.py
@@ -16,14 +16,32 @@ class Goal:
         self.name = name
         self.deadline = deadline
         self.doc_id = None
+        self.tags = []
 
     def __str__(self):
         return f"{self.name} with deadline: {self.deadline}"
-    
+
+    def add_tag(self, tag):
+        if type(tag) is str:
+            self.tags.append(tag)
+            self.replace()
+        else:
+            raise Exception('tag must be a string')
+
+    def to_doc(self):
+        return {"name": self.name, "deadline": self.deadline, "tags": self.tags}
+
+    def replace(self):
+        update_result = self.repo.replace_one(
+            {"_id": self.doc_id}, self.to_doc())
+        return update_result.matched_count
+
+
     def save(self):
-        doc = {"name": self.name, "deadline": self.deadline}
+        doc = self.to_doc()
         self.doc_id = self.repo.insert_one(doc).inserted_id
         return self.doc_id
+
 
     def delete(self):
         if self.doc_id is not None:
@@ -37,5 +55,10 @@ if __name__ == "__main__":
     goals = Goal.get_all()
     for goal in goals:
         pprint.pprint(goal)
+    mongo_goal.add_tag("test")
+    goals = Goal.get_all()
+    for goal in goals:
+        pprint.pprint(goal)
+
     mongo_goal.delete()
     
