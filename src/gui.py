d76249ae83175d1cc83c1e8e84b6d5fe89062f1b
kuberlog <collinalexbell@gmail.com>
2021-02-23 20:29:02 -0600

    objective_widget added

--- src/gui.py
+++ src/gui.py
@@ -5,6 +5,7 @@ from gi.repository import Gtk
 
 from add_goal_widget import AddGoalWidget
 from manage_existing_goals_widget import ManageExistingGoalsWidget
+from objective_widget import ObjectiveWidget
 
 class GoalsWindow(Gtk.Window):
     def __init__(self, repo):
@@ -17,6 +18,21 @@ class GoalsWindow(Gtk.Window):
         self.main_box = Gtk.VBox()
         self.add(self.main_box)
 
+        self.objectives_widget = ObjectiveWidget(
+            [
+            "Determination",
+            "Cybernetics",
+            "Business",
+            "Finance",
+            "Health",
+            "Lifestyle",
+            "Politics",
+            "Reading",
+            "Social"
+            ], 
+            self.select_objective_callback)
+        self.main_box.add(self.objectives_widget)
+
         self.manage_existing_goals_widget = ManageExistingGoalsWidget(self.delete_goal_callback)
         for goal in self.goals_from_repo:
             self.manage_existing_goals_widget.add_to_goal_list(goal)
@@ -26,6 +42,9 @@ class GoalsWindow(Gtk.Window):
         self.main_box.pack_end(self.add_goal_widget, True, True, 0)
 
         self.show_all()
+
+    def select_objective_callback(self):
+        pass
 
     def add_goal_callback(self, goal):
         self.manage_existing_goals_widget.add_to_goal_list(goal)
